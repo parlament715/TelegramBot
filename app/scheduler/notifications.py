73b3cce2fa94b2ba9689e5592b354cea22bdb6ed
parlament700 <alexfresh700@gmail.com>
2024-12-16 11:27:17 +0700

    notifications schedule

--- app/scheduler/notifications.py
+++ app/scheduler/notifications.py
@@ -8,21 +8,21 @@ def add_job_scheduler(scheduler):
     datt_now = datetime.now()
     datt = datetime(datt_now.year, datt_now.month,
                     datt_now.day, time_to.hour, time_to.minute, 0)
-    # scheduler.add_job(CheckRemainders, 'cron', day_of_week="1-5", hour=(datt - timedelta(minutes=5)).hour,
+    # scheduler.add_job(CheckRemainders, 'cron', day_of_week="0-5", hour=(datt - timedelta(minutes=5)).hour,
     #                   minute=(datt - timedelta(minutes=5)).minute)
-    scheduler.add_job(send_to_teacher, 'cron', day_of_week="1-5", hour=time_from.hour,
+    scheduler.add_job(send_to_teacher, 'cron', day_of_week="0-5", hour=time_from.hour,
                       minute=time_from.minute, args=["запись открыта"])
-    scheduler.add_job(send_to_teacher, 'cron', day_of_week="1-5", hour=time_to.hour,
+    scheduler.add_job(send_to_teacher, 'cron', day_of_week="0-5", hour=time_to.hour,
                       minute=time_to.minute, args=["запись закрыта"])
-    scheduler.add_job(send_to_vosp, 'cron', day_of_week="1-5", hour=time_from.hour,
+    scheduler.add_job(send_to_vosp, 'cron', day_of_week="0-5", hour=time_from.hour,
                       minute=time_from.minute, args=["запись открыта"])
-    scheduler.add_job(send_to_vosp, "cron", day_of_week="1-5", hour=time_to.hour,
+    scheduler.add_job(send_to_vosp, "cron", day_of_week="0-5", hour=time_to.hour,
                       minute=time_to.minute, args=["запись закрыта"])
-    scheduler.add_job(send_notifications, "cron", day_of_week="1-5", hour=(datt - timedelta(minutes=45)).hour,
+    scheduler.add_job(send_notifications, "cron", day_of_week="0-5", hour=(datt - timedelta(minutes=45)).hour,
                       minute=(datt - timedelta(minutes=45)).minute)
-    scheduler.add_job(send_notifications, "cron", day_of_week="1-5", hour=16,
+    scheduler.add_job(send_notifications, "cron", day_of_week="0-5", hour=16,
                       minute=0)
-    scheduler.add_job(send_notifications_teacher, "cron", day_of_week="1-5", hour=12,
+    scheduler.add_job(send_notifications_teacher, "cron", day_of_week="0-5", hour=12,
                       minute=1)
-    scheduler.add_job(send_notifications_teacher, "cron", day_of_week="1-5", hour=13,
+    scheduler.add_job(send_notifications_teacher, "cron", day_of_week="0-5", hour=13,
                       minute=6)
